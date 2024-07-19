62804c60e5b86443843652a6b1a3ecb4694eb077
jayakumar <jayekumar12@gmail.com>
2024-07-20 01:23:27 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,17 @@
+
+function show(){
+    var who=document.getElementById("who")
+    .style .display="block"
+}
+function show1(){
+    var skills=document.getElementById("skills")
+    .style.display="block"
+}
+function show2(){
+    var edu=document.getElementById("edu")
+    .style.display="block"
+}
+function show3(){
+    var details=document.getElementById("details")
+    .style.display="block"
+}
